7eff39587d583f6abd7fbaa99af7dd5bca8a7fa2
itoshin1341 <82028845+itoshin1341@users.noreply.github.com>
2021-05-10 19:05:47 +0900

    Introducing ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,14 @@ class Item < ApplicationRecord
 
   belongs_to :user
   belongs_to :purchaser_history
+
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions  
+  belongs_to :category
+  belongs_to :product_stats
+  belongs_to :delivery_change_burden
+  belongs_to :prefecture
+  belongs_to :delivery_date
 
 end
