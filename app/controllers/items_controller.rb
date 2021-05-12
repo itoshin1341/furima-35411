36d43b500fa3f45ce559fc28d83d60479bddcb28
itoshin1341 <82028845+itoshin1341@users.noreply.github.com>
2021-05-12 21:56:25 +0900

    Fix create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     # 全ての商品情報を出力できるように定義
