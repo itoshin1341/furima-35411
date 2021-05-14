54974fdd51b63a6b443e871cd93ec7bb84632a27
itoshin1341 <82028845+itoshin1341@users.noreply.github.com>
2021-05-14 19:32:40 +0900

    Product information editing function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,12 +23,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
   private
 
   def item_params
     params.require(:item).permit(:content, :image, :category_id, :product_stats_id, :delivery_change_burden_id,
                                  :prefecture_id, :delivery_date_id, :product_name, :product_description, :price)
                          .merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 
 end
